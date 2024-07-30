7f844974b42ed11005055fba09284ee46c0f175d
Joanna Rodriguez <joannarodriguez134@gmail.com>
2024-07-30 17:42:45 -0500

    Initial Commit

---
+++ TinderClone/TinderCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinderCloneApp.swift
+//  TinderClone
+//
+//  Created by Joanna Rodriguez on 7/30/24.
+//
+
+import SwiftUI
+
+@main
+struct TinderCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
